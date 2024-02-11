50237daad582916cdd668658c2efae7360602491
Jhon Sabogal <JhonSabogal11@outlook.com>
2024-02-11 09:21:10 -0500

    First commit

---
+++ src/script.js
@@ -0,0 +1,4 @@
+document.getElementById('button_1').addEventListener('click', function() {
+    var columnasTexto = document.querySelector('.column-hidden'); // Cambiado aquí
+    columnasTexto.classList.toggle('show');
+});
