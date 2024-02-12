59c594a30dd79423be2e3a1b7004c32605646477
Jhon Sabogal <JhonSabogal11@outlook.com>
2024-02-11 19:26:07 -0500

    Updating index.html

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
 document.getElementById('button_1').addEventListener('click', function() {
-    var columnasTexto = document.querySelector('.column-hidden'); // Cambiado aquí
+    var columnasTexto = document.querySelector('.column-hidden');
     columnasTexto.classList.toggle('show');
 });
