2875c829a3180f4fa7f53679a5d1c27f33bfab86
Jhon Sabogal <JhonSabogal11@outlook.com>
2024-02-14 18:27:08 -0500

    Updating CSS

--- src/script.js
+++ src/script.js
@@ -1,4 +1,16 @@
 document.getElementById('button_1').addEventListener('click', function() {
     var columnasTexto = document.querySelector('.column-hidden');
     columnasTexto.classList.toggle('show');
+
+    
+    if (columnasTexto.classList.contains('show')) {
+        scrollToBottom();
+    }
 });
+
+function scrollToBottom() {
+    window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: 'smooth'
+    });
+}
